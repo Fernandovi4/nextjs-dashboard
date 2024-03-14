361ba0ad5b91f92df85550373df6fffa2997c159
Petro Rikabal Aviles <Petro.Aviles@Starz.com>
2024-03-14 21:57:18 +0100

    updated tsConfig

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import '@/app/ui/global.css';
+import React from "react";
+import { inter } from '@/app/ui/fonts';
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+}
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana  } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], weight: "800" });
+export const lusitana  = Lusitana ({ subsets: ['latin'], weight: "700" });
